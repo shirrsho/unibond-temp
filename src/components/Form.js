2c271137d3a3ac41e57aaa53893bb940ad667e80
Hasnain Iqbal <60136067+shirsho1106@users.noreply.github.com>
2023-04-04 19:53:35 +0600

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -121,7 +121,7 @@ export default function Form() {
             {/* </div> */}
             {submitted===false && <div style={{ width: "max-content" }}>
                 <p className='text-muted' style={{ marginTop: "-2%", padding: "10px" }}>
-                Please pay the booking fee <strong>100Tk</strong> via Bkash or Rocket.<br />Save the Transaction ID and put that into the respective field.
+                Please pay the booking fee <strong>100Tk</strong> via Bkash or Rocket.<br />Save the Transaction ID and put that into its respective field.
                     <br /><b>Bkash:</b> 01707368255<br/>
                     <span style={{color:"red"}}>Note: Any incorrect information can lead to order cancelation.</span></p>
                 <div>
